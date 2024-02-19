6d7a41ae8174ddc544d1648782c8278efb3bbcf6
ashra <ashrafulkhan194@gmail.com>
2024-02-19 19:14:31 +0600

    Changed

---
+++ bin/operators.dart
@@ -0,0 +1,23 @@
+main() {
+  int a = 23+ 45;
+  int b = 34 - 5;
+  int c = 454 * 34;
+  double d = 35 / 2;
+  int e = 23 % 4;
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+  print(e);
+
+  //a = a + 1;
+  a++;
+  //a= a - 1;
+  a--;
+  ++a;
+  --a;
+
+  a+= 10; //a = a + 10;
+  a-= 12; //a = a - 12;
+  a*= 2; //a = a * 2;
+}
