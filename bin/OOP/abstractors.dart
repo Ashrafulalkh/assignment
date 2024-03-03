00cd06b7ad6d965c8d575174fb98e505aa4014b7
ashra <ashrafulkhan194@gmail.com>
2024-03-04 00:41:19 +0600

    Changed

---
+++ bin/OOP/abstractors.dart
@@ -0,0 +1,58 @@
+abstract class Student {
+  void reading();
+  void classJoining();
+  void talking() {
+    print('Taking with other students');
+  }
+}
+
+class CSEStudent extends Student {
+  @override
+  void reading() {
+    print('CSE Student Read Codes');
+  }
+
+  @override
+  void classJoining() {
+    print('Class Join Through Google Meet');
+  }
+
+}
+
+class Biology extends Student {
+  @override
+  void reading() {
+    print('Biology Students Read Biology');
+  }
+
+  @override
+  void classJoining() {
+    print('CLass Join In Physically');
+  }
+}
+
+class English extends Student {
+  @override
+  void reading() {
+    print('English Students Read English');
+  }
+
+  @override
+  void classJoining() {
+    print('CLass Join in field');
+  }
+}
+
+void main() {
+  CSEStudent cseStudentOne = CSEStudent();
+  cseStudentOne.reading();
+  cseStudentOne.classJoining();
+
+  Biology biologyOne = Biology();
+  biologyOne.reading();
+  biologyOne.classJoining();
+
+  English englishOne = English();
+  englishOne.reading();
+  englishOne.classJoining();
+}
